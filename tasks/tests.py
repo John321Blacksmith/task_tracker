a576674bdf91b5e86b2cfb08cf06762c451d7986
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-27 14:55:15 +0300

    trivial coverage by test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from .models import Category, Task
 
 # Create your tests here.
+
+
+class TestTaskLifeSpan(TestCase):
+    """
+    Test basic data manipulation.
+    """
+    @classmethod
+    def setUp(cls):
+        [Category.objects.create(title=title) for title in ['home', 'cooking', 'study']]
+        cls.study_task = Task.objects.create(
+            title='Prepare for lessons',
+            description='Take the book from the library and give it back today by 19pm',
+        )
+        cls.home_task = Task.objects.create(
+            title='Go for a walk with my pets',
+            description='Let my dad know I\'ll go out with our dog'
+        )
+        cls.cooking_task = Task.objects.create(
+            title='Make a dinner',
+            description='shoot out for meat, vegetables, salt to the market'
+        )
+        cls.cat_titles = [cat_title for cat_title in Category.objects.values('title')]
+        
+    def test_tasks_have_right_category(self):
+        self.assertEqual(self.study_task.category.title, 'study')
+        self.assertEqual(self.home_task.category.title, 'home')
+        self.assertEqual(self.cooking_task.category.title, 'cooking')
+        self.assertNotIn('other', self.cat_titles)
+    
+
+class TestSerializationProcess(TestCase):
+    @classmethod
+    def setUp(cls):
+        ...
