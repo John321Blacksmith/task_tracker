7fa3ea1ad4b9542a71d45dd1adade52b50bf9295
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-27 17:21:38 +0300

    dataset was updated

--- datasets.py
+++ datasets.py
@@ -1,6 +1,6 @@
 en_categories = {
     'sport': {'run', 'water', 'go', 'exercise', 'rest', 'muscle', 'gym'},
-    'home': {'clean', 'family', 'care', 'cook', 'pets', 'dad', 'room', 'flat', 'home', 'dog' 'house', 'children', 'child', 'son', 'daughter'},
+    'home': {'clean', 'family', 'care', 'cook', 'pets', 'room', 'flat', 'home', 'house', 'children', 'child', 'son', 'daughter'},
     'work': {'collegues', 'weekday' 'work', 'boss', 'director', 'office', 'hour', 'tommorow', 'yesterday', 'deadline', 'finish'},
     'cooking': {'water', 'cook', 'dinner', 'breakfast', 'lunch', 'meal', 'tasty', 'family', 'kitchen'},
     'study': {'project', 'learn', 'library', 'information', 'by', 'heart'},
