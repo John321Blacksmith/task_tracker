e4e4b1c3dd0c9606a088a5d554a6f43a31c68b8f
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-27 14:54:34 +0300

    Task model has been fixed

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datasets import en_categories
 # Create your models here.
 
 
@@ -27,12 +27,12 @@ class Task(models.Model):
         all_literals = set()
         for l in [f.split(' ') for f in self.__dict__.values() if isinstance(f, str)]:
             for w in l:
-                all_literals.add(w)
+                all_literals.add(w.lower())
         return all_literals
     
-    def get_category(self, dataset: dict[str, set]) -> str | None:
-        stats = {k: len(self.literal_data & v) for k, v in dataset.items()}
-        tups = [(k, v) for k, v in stats.items() if v > 0]
+    def get_category(self, dataset: dict[str, set[str]]) -> str | None:
+        stats = {k: len(self.literal_data & v) for k, v in dataset.items()} # record stats
+        tups = [(k, v) for k, v in stats.items() if v > 0] # tuples for sorting stats
         if len(tups) > 0:
             possible_category = tups[0]
             for i in range(0, len(tups)):
@@ -41,12 +41,11 @@ class Task(models.Model):
             return possible_category[0]
         return None
         
-    
-    def save(self):
-        category = self.get_category(...)
-        if category:
-            self.category = Category.objects.get_or_create(title=category)[0]
-        else:
+    def save(self, *args, **kwargs):
+        category = self.get_category(en_categories)
+        try:
+            self.category = Category.objects.get(title=category)
+        except Category.DoesNotExist:
             self.category = Category.objects.get_or_create(title='other')[0]
-    
-        return super().save()
+
+        super().save(*args, **kwargs)
