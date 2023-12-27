9f2136518d21839d74c4975c7ca9416eb5a00480
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-27 14:55:37 +0300

    added datasets

---
+++ datasets.py
@@ -0,0 +1,17 @@
+en_categories = {
+    'sport': {'run', 'water', 'go', 'exercise', 'rest', 'muscle', 'gym'},
+    'home': {'clean', 'family', 'care', 'cook', 'pets', 'dad', 'room', 'flat', 'home', 'dog' 'house', 'children', 'child', 'son', 'daughter'},
+    'work': {'collegues', 'weekday' 'work', 'boss', 'director', 'office', 'hour', 'tommorow', 'yesterday', 'deadline', 'finish'},
+    'cooking': {'water', 'cook', 'dinner', 'breakfast', 'lunch', 'meal', 'tasty', 'family', 'kitchen'},
+    'study': {'project', 'learn', 'library', 'information', 'by', 'heart'},
+    
+
+}
+
+ru_categories ={
+    'спорт': {'бег', 'вода', 'идти', 'упражнения', 'отдых', 'мышцы', 'тренажерный зал'},
+    'дом': {'чисто', 'семья', 'забота', 'готовить', 'комната', 'квартира', 'дом', 'дом', 'дети', 'ребенок', 'сын', ' дочь'},
+    'работа': {'коллеги', 'будний день' 'работа', 'начальник', 'директор', 'офис', 'час', 'завтра', 'вчера', 'крайний срок', 'окончание'},
+    'готовка': {'вода', 'готовить', 'ужин', 'завтрак', 'обед', 'еда', 'вкусно', 'семья', 'кухня'},
+    'учеба': {'проект', 'учиться', 'библиотека', 'исследование', 'информация', 'наизусть'}
+}
