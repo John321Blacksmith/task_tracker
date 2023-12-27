3af69341f7d1b5fe57e9af13de8014cc6d5a658e
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-27 13:44:25 +0300

    fix

--- tasks/models.py
+++ tasks/models.py
@@ -22,6 +22,14 @@ class Task(models.Model):
         verbose_name = 'task'
         verbose_name_plural = 'tasks'
         
+    @property
+    def literal_data(self) -> set:
+        all_literals = set()
+        for l in [f.split(' ') for f in self.__dict__.values() if isinstance(f, str)]:
+            for w in l:
+                all_literals.add(w)
+        return all_literals
+    
     def get_category(self, dataset: dict[str, set]) -> str | None:
         stats = {k: len(self.literal_data & v) for k, v in dataset.items()}
         tups = [(k, v) for k, v in stats.items() if v > 0]
@@ -33,13 +41,6 @@ class Task(models.Model):
             return possible_category[0]
         return None
         
-    @property
-    def literal_data(self) -> set:
-        all_literals = set()
-        for l in [f.split(' ') for f in self.__dict__.values() if isinstance(f, str)]:
-            for w in l:
-                all_literals.add(w)
-        return all_literals
     
     def save(self):
         category = self.get_category(...)
