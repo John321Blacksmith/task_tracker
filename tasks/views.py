b8a1ffa47cc1aca77dedc5fb6b69e6695bb58aa1
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-28 16:19:12 +0300

    added detail view

--- tasks/views.py
+++ tasks/views.py
@@ -1,21 +1,42 @@
 from django.http import Http404
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.mixins import ListModelMixin
-from rest_framework.generics import GenericAPIView
-from .models import Task, Category
-from .serializers import TasksListSerializer
+from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
+from .models import Task
+from .serializers import TasksListSerializer, TaskDetailSerializer, TaskCreationSerializer
 
 # Create your views here.
 
-class TasksListAPIView(ListModelMixin, GenericAPIView):
+class TasksListAPIView(GenericAPIView):
     """
     This view represents a
     list of tasks with the
     defined fields.
     """
     queryset = Task.objects.all()
-    renderer_classes = [JSONRenderer]
     serializer_class = TasksListSerializer
+    renderer_classes = [JSONRenderer]
+    
     
     def get(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        tasks = self.get_queryset()
+        serializer = self.serializer_class(tasks, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = TaskCreationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
+
+class TaskDetailAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    This view shows up a task
+    and facilitates RUD methods
+    """
+    queryset = Task.objects.all()
+    renderer_classes = [JSONRenderer]
+    serializer_class = TaskDetailSerializer
