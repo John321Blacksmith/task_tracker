63420f7a02f3944204dca1e0626a0980ae8a5be6
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-12-28 16:18:38 +0300

    added detail serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -25,3 +25,40 @@ class TasksListSerializer(serializers.ModelSerializer):
     
     def get_category_title(self, obj):
         return obj.category.title
+    
+
+class TaskCreationSerializer(serializers.ModelSerializer):
+    """
+    Serialize fields for
+    task creation.
+    """
+    class Meta:
+        model = Task
+        fields = [
+            'title',
+            'description'
+        ]
+
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    """
+    Prepare & serialize data for either
+    task observation or update.
+    """
+    status = serializers.SerializerMethodField('get_status')
+    category = serializers.SerializerMethodField('get_category_name')
+    class Meta:
+        model = Task
+        fields = [
+            'category',
+            'title',
+            'description',
+            'date_published',
+            'status'
+        ]
+    
+    def get_category_name(self, obj):
+        return obj.category.title
+    
+    def get_status(self, obj):
+        return 'active' if obj.is_active else 'inactive'
