8edd3e90c495e94f699c23ce9f9bdd1884fba5a1
John321Blacksmith <kuzmichevi321@yandex.ru>
2024-07-05 11:10:51 +0300

    models were added

--- tasks/models.py
+++ tasks/models.py
@@ -8,11 +8,11 @@ class Category(models.Model):
     Tis model represents a
     category object.
     """
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, verbose_name='Category')
 
     class Meta:
-        verbose_name  = 'category'
-        verbose_name_plural = 'categories'
+        verbose_name  = 'Category'
+        verbose_name_plural = 'Categories'
     
     def __str__(self):
         return self.title
@@ -20,74 +20,74 @@ class Category(models.Model):
 
 class Task(models.Model):
     """
-    This model represents a
-    task object.
+    This model represents an
+    abstract task interface.
     """
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    date_published = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name='Task title')
+    description = models.TextField(verbose_name='Description')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    is_completed = models.BooleanField(default=False, verbose_name='Completion state')
     
     class Meta:
-        verbose_name = 'task'
-        verbose_name_plural = 'tasks'
+        abstract = True
+
+
+class SimpleTask(Task):
+    """
+    This model represents a
+    regular standalone task
+    object.
+    """
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+    due_date = models.DateTimeField(verbose_name='Should be finished by')
+    
+    class Meta:
+        verbose_name = 'Simple Task'
+        verbose_name = 'Simple Tasks'
     
     def __str__(self):
-        return self.title
-    
-    @property
-    def literal_data(self) -> set:
-        """
-        All the literal string
-        data is collected in
-        the set.
-        """
-        all_literals = set()
-        for l in [f.split(' ') for f in self.__dict__.values() if isinstance(f, str)]:
-            for w in l:
-                all_literals.add(w.lower())
-        return all_literals
-    
-    def get_category(self, dataset: dict[str, set[str]]) -> str | None:
-        """
-        Analyze the collected string
-        data and define what category
-        the task is mostly of.
-        Params:
-            :dataset: dict[str, set[str]] - a collection of patterns
-        Output:
-            :category_name: str | None - probable category
-        """
-        object_set = set() # only the contained patterns
-        for cat in dataset.keys():
-            for lit_v in self.literal_data:
-                if len(lit_v) > 2:
-                    for pattern in dataset[cat]:
-                        if pattern in lit_v:
-                            object_set.add(pattern)
-        # the pattern comp is then compared again
-        stats = {k: len(object_set & v) for k, v in dataset.items()} # record stats
-        tups = [(k, v) for k, v in stats.items() if v > 0] # tuples for sorting stats
-        
-        if len(tups) > 0:
-            possible_category = tups[0]
-            for i in range(0, len(tups)):
-                if tups[i][1] > possible_category[1]:
-                    possible_category = tups[i]
-            return possible_category[0]
-        return None
-        
-    def save(self, *args, **kwargs):
-        """
-        Save the task under an
-        existing category or
-        refer it to `other`.
-        """
-        category = self.get_category(categories)
-        try:
-            self.category = Category.objects.get(title=category)
-        except Category.DoesNotExist:
-            self.category = Category.objects.get_or_create(title='other')[0]
+        return f'#{self.pk} {self.title}'
 
-        super().save(*args, **kwargs)
+
+class Project(models.Model):
+    """
+    This model represents a
+    project object.
+    """
+    title = models.CharField(max_length=256, verbose_name='Project title')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
+    
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        
+
+class Sprint(models.Model):
+    """
+    This model represents a
+    chunk of time within which
+    the tasks can be managed.
+    """
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Included to project: ')
+    started_at = models.DateTimeField(auto_now_add=True, verbose_name='Start date')
+    ends_at = models.DateTimeField(verbose_name='Finish date')
+    
+    class Meta:
+        verbose_name = 'Sprint'
+        verbose_name_plural = 'Sprints'
+        
+
+class SprintTask(Task):
+    """
+    This model extends the
+    task which the sprint
+    contains
+    """
+    sprint = models.ForeignKey(Sprint, on_delete=models.CASCADE, verbose_name='Included to sprint')
+
+    class Meta:
+        verbose_name = 'Sprint Task'
+        verbose_name_plural = 'Sprint Tasks'
+        
+    def __str__(self):
+        return f'#{self.pk} {self.title}'
