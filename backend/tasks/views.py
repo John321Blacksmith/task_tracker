85ff9d994e915f63e1dd47b120ad2c7d5378c9d3
John321Blacksmith <kuzmichevi321@yandex.ru>
2024-07-24 12:39:38 +0300

    tasks are returned with an only list of categories

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import SimpleTask, Project, Sprint, SprintTask
+from .models import Category, SimpleTask, Project, Sprint, SprintTask
 from .serializers import (
     SimpleTasksListSerializer, ProjectSerializer,
-    SprintSerializer, SprintTaskSerializer
+    SprintSerializer, SprintTaskSerializer,
+    CategorySerializer
 )
 
 # Create your views here.
@@ -29,8 +30,31 @@ class SimpleTaskViewSet(ModelViewSet):
         'is_completed'
     ]
     
+    def list(self, request, *args , **kwargs):
+        """
+        Overwritten list representaion
+        method which returns a response
+        with a list of simple tasks and
+        one, and only one set of categories.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        
+        categories_serializer = CategorySerializer(Category.objects.all(), many=True)
+        
+        if page is not None:
+            tasks_serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(
+                data={
+                    'tasks': tasks_serializer.data,
+                    'categories': categories_serializer.data
+                    }
+                )
+        
+        tasks_serializer = self.get_serializer(queryset, many=True)
+        
+        return Response(data={'tasks': tasks_serializer.data, 'categories': categories_serializer.data})
     
-
 
 class ProjectViewSet(ModelViewSet):
     """
